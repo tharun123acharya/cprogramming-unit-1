e46117c82a4544989f511c9a5184cdde2bbf0da9
tharun <tharun.123acharya@gmail.com>
2025-11-25 08:20:41 +0530

    Add files via upload

---
+++ pointerarithmetic.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int arr[] = {5, 10, 15, 20, 25};
+    int *ptr;
+
+    ptr = arr;
+
+    printf("ptr = %p, *ptr = %d\n", ptr, *ptr);
+
+    
+
+    printf("after ptr++, ptr = %p, *ptr = %d\n", ptr, *ptr);
+ptr+=2;
+printf("after ptr+=2;ptr=%p,*ptr=%d\n",ptr,*ptr);
+    return 0;
+}
