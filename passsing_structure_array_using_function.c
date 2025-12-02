87331268a53f224bcfb35b58c1d0f52eaaeb9fb6
tharun <tharun.123acharya@gmail.com>
2025-12-02 08:44:59 +0530

    Add files via upload

---
+++ passsing_structure_array_using_function.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+struct student {
+    char name[20];
+    int id;
+};
+
+void display(struct student s[], int n) {
+    for(int i = 0; i < n; i++) {
+        printf("Student %d -> Name: %s, ID: %d\n", i+1, s[i].name, s[i].id);
+    }
+}
+
+int main() {
+    struct student students[3] = {
+        {"Tejas", 136},
+        {"Balaji", 110},
+        {"Shreesh", 141}
+    };
+
+    display(students, 3);
+
+    return 0;
+}
